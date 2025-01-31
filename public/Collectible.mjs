cdcbb61a186a5da9ce119f575198dbd76d449bd2
Simon Bouchard <simon.bouchard31@gmail.com>
2025-01-31 16:47:44 -0500

    add collectible logic

--- public/Collectible.mjs
+++ public/Collectible.mjs
@@ -1,7 +1,33 @@
-class Collectible {
-  constructor({x, y, value, id}) {
+const canvas = document.getElementById('game-window');
 
-  }
+class Collectible {
+	constructor({x, y, value=1, id=1} = {}) {
+
+		if (!x && !y) {
+			this.reset()
+		} else {
+			this.x = x
+			this.y = y
+		}
+
+		this.value = value
+		this.id = id
+
+  	}
+
+	reset() {
+		const padding = 5;
+		const gameArea = {
+	    	x: 5,
+		    y: 50,
+			width: 640 - 10,
+		    height: 480 - 55
+		};
+		
+
+		this.x = Math.floor(Math.random() * (gameArea.width - padding * 2)) + gameArea.x + padding;
+	    this.y = Math.floor(Math.random() * (gameArea.height - padding * 2)) + gameArea.y + padding;
+	}
 
 }
 
