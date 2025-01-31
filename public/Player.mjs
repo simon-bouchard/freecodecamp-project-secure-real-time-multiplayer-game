84bfcc22ee87a5d91e15d7fa8a1bea52fc9a04b9
Simon Bouchard <simon.bouchard31@gmail.com>
2025-01-31 16:47:13 -0500

    add player logic

--- public/Player.mjs
+++ public/Player.mjs
@@ -1,19 +1,53 @@
 class Player {
-  constructor({x, y, score, id}) {
+  	constructor({x = 320, y = 240, score = 0, id='new'} = {}) {
 
-  }
+	  	this.x = x;
+	  	this.y = y
+	  	this.score = score;
+	  	this.id = id
+  	}
 
-  movePlayer(dir, speed) {
+  	movePlayer(dir, speed) {
 
-  }
+		if (dir === 'up') {
+			this.y -= speed;
+		}
+		if (dir === 'down') {
+			this.y += speed;
+		}
+		if (dir === 'left') {
+			this.x -= speed;
+		}
+		if (dir === 'right') {
+			this.x += speed;
+		}
+  	}
 
-  collision(item) {
+  	collision(item) {
+		if (
+			this.x < item.x + 20 &&
+			this.x + 20 > item.x &&
+			this.y < item.y + 20 &&
+			this.y + 20 > item.y 
+		) {
+			this.score += item.value;
+			console.log(this.score);
+			item.reset();
+		}
+  	}
 
-  }
+  	calculateRank(arr) {
+		let currentRanking = 0;
+		let totalPlayers = 0;
 
-  calculateRank(arr) {
+	  	for (var player=0; player < arr.length; player ++){
+			if (arr[player].score > this.score) {
+				currentRanking += 1;
+			}
+		}
 
-  }
+		return `Rank: ${currentRanking}/${player}`
+  	}
 }
 
 export default Player;
